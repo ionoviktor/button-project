a76351c37594454020520b508867b3cbe8b5a501
Viktor <33.1_tm@mail.ru>
2022-01-21 21:43:55 +0600

    Траляля

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 
-const buttons = document.querySelector('.buttons');
-const popUpWindow = document.querySelector('.popUpWindow');
+const buttons = document.querySelectorAll('button'),
+      buttonsWrapper = document.querySelector('.buttonsWrapper')
+      popUpWindow = document.querySelector('.popUpWindow'),
+      popUpContent = document.querySelector('.popUpContent');
 
 
+// buttonsWrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.tagName == "BUTTON") {
+//         console.log('Hello');
+//     }
+// });
 
 function showMessageIsButtonClick() {
-    buttons.addEventListener('click', (event) => {
-        console.log(event);
-        showPopUpWindow();
-        showMessageInWindow();
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            showPopUpWindow();
+            showMessageInWindow();
+            closePopUpWindow();
+            console.log(button.className);
+        })
     })
 }
 
 
 function showMessageInWindow() {
-    const popUpContent = document.querySelector('.popUpContent')
-
     
-    // let buttonClass = buttons.target.classList[0];
-    // console.log(`Вы нажали на ${buttonClass} кнопку.`);
 }
 
 function closePopUpWindow() {
@@ -36,6 +42,4 @@ function hidePopUpWindow() {
     popUpWindow.style.display = "none";
 }
 
-showMessageInWindow();
-closePopUpWindow();
 showMessageIsButtonClick();
